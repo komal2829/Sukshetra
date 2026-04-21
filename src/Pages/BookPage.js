cd6bbeffc1d764772f5cce1539a021e4ff85fa87
komal2829 <komsred@gmail.com>
2026-04-21 17:42:36 +0530

    fix

--- src/Pages/BookPage.js
+++ src/Pages/BookPage.js
@@ -30,7 +30,7 @@ export default function BookPage() {
     try {
       console.log("API URL:", process.env.REACT_APP_API_URL);
 
-    const res = await axios.get(`${API_URL}/api/bookings`);
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/bookings`)
       setBookings(res.data || []);
     } catch (err) {
       console.error("fetch bookings error", err);
